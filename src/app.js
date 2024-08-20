7d66df94d9f99251b4b42003085b9d36fca8a595
Rebekah Banks <168665115+Banksrm75@users.noreply.github.com>
2024-08-20 17:14:40 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generateExcuse();
+  function generateExcuse() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * max);
+    }
+
+    let newExcuse =
+      who[getRandomInt(who.length)] +
+      " " +
+      action[getRandomInt(action.length)] +
+      " " +
+      what[getRandomInt(what.length)] +
+      " " +
+      when[getRandomInt(when.length)];
+    return newExcuse;
+  }
 };
