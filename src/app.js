64510129cfdb1ee8ade5d5a45341927710f7b723
Rebekah Banks <168665115+Banksrm75@users.noreply.github.com>
2024-08-20 18:12:23 +0000

    Firstcommit

--- src/app.js
+++ src/app.js
@@ -8,15 +8,23 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   document.getElementById("excuse").innerHTML = generateExcuse();
   function generateExcuse() {
-    let who = ["The dog", "My grandma", "The mailman", "My bird"];
-    let action = ["ate", "peed", "crushed", "broke"];
-    let what = ["my homework", "my phone", "the car"];
+    let who = [
+      "The dog",
+      "My grandma",
+      "The mailman",
+      "E.T.",
+      "My bird",
+      "My husband"
+    ];
+    let action = ["ate", "peed", "crushed", "broke", "took"];
+    let what = ["my homework", "my phone", "the car", "my computer"];
     let when = [
       "before the class",
       "when I was sleeping",
       "while I was exercising",
       "during my lunch",
-      "while I was praying"
+      "while I was praying",
+      "this morning"
     ];
 
     function getRandomInt(max) {
